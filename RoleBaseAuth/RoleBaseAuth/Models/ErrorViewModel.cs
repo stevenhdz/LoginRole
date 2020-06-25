601da92997fa8e758fd752c9590e0f3933ea2489
Alexander Jimenez <stevenhernandezj@gmail.com>
2020-06-25 00:31:48 -0500

    login valida admin or empleado
    valida admin or empleado

---
+++ RoleBaseAuth/RoleBaseAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoleBaseAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
